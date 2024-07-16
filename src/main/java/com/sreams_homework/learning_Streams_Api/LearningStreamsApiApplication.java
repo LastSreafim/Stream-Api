d13f519e4ae747f8584185e9ef822deb14c1985d
Nazar <epoxasmute@mail.ru>
2024-07-16 12:02:47 +0300

    Git init

---
+++ src/main/java/com/sreams_homework/learning_Streams_Api/LearningStreamsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sreams_homework.learning_Streams_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningStreamsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningStreamsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sreams_homework/learning_Streams_Api/LearningStreamsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreams_homework.learning_Streams_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningStreamsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
